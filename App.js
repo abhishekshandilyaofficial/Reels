9bbc11f776b79fd78e9cca2208644b74f94eb3dc
abhishekshandilyaofficial <abhishekshandilyaofficial@gmail.com>
2022-11-21 00:11:03 +0530

    Added upto private route

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React,{ useContext } from 'react';
 import Signup from './component/Signup'
 import Feed from './component/Feed'
 import Login from './component/Login'
 import Forget from './component/Forget'
 import PageNotFound from './component/PageNotFound'
 import Profile from './component/Profile'
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import { AuthContextProvider } from './context/AuthContext';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import { AuthContextProvider, AuthContext } from './context/AuthContext';
 function App() {
   return (
    <>
    <BrowserRouter>
         <AuthContextProvider>
               <Switch>
-            <Route path="/feed">
-            <Feed></Feed>
-            </Route>
+            
             <Route path="/login">
             <Login></Login>
             </Route>
@@ -25,9 +23,12 @@ function App() {
             <Route path="/forget">
             <Forget></Forget>
             </Route>  
-            <Route path="/profile">
-            <Profile></Profile>  
-            </Route>
+            <PrivateRoute path="/profile"
+            comp={<Profile></Profile> }>
+            </PrivateRoute>
+            <PrivateRoute path="/feed"
+            comp={<Feed></Feed>}>
+            </PrivateRoute>
             <Route>
             <PageNotFound></PageNotFound>
             </Route>
@@ -37,6 +38,26 @@ function App() {
     </BrowserRouter>
    </>
   );
+}
+//what is the use of this private route
+  //First - check -> are you logged in
+  //Second - based on login details if null - send to login page 
+  //else the page which is requested
+function PrivateRoute(){  
+  let Component = props.comps;
+  let cUser = useContext(AuthContext);
+  return (
+    <Route
+    {...props}
+    render={
+     (props) =>{
+      //logic
+      return cUser != null ? <Component {...props}></Component>:<Redirect {...props} to="/login"></Redirect>
+     } 
+    }
+    ></Route>
+  )
+  // 
 }
 
 export default App;
